af59d218b4b529903ad03ecc8585601ae308aba4
Milan Tyagi <milantyagi2004@gmail.com>
2024-12-08 09:25:41 +0530

    creating server folder and user interface

---
+++ personaldairy-server/src/main/java/com/PersonalDairy_server/PDairy/PDairyApplication.java
@@ -0,0 +1,13 @@
+package com.PersonalDairy_server.PDairy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PDairyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PDairyApplication.class, args);
+	}
+
+}
